1377bd097fd2ce78cf59606665eb11defa9340d2
MuskChan <523919565@qq.com>
2019-09-21 10:25:38 +0800

    init

---
+++ database/migrations/2019_05_07_085051_seed_categories_data.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SeedCategoriesData extends Migration
+{
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '分享',
+                'description' => '分享创造，分享发现',
+            ],
+            [
+                'name'        => '人民日报文摘',
+                'description' => '人民日报文摘、人民日报读后感等',
+            ],
+            [
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://twitter.com/chan_musk" target="_blank">Musk Chan</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+    <p class="float-right">
+      <strong>一个人民日报死忠粉</strong>
+      <a href="mailto:{{ setting('contact_email') }}">联系我们</a>
+    </p>
+  </div>
+</footer>
